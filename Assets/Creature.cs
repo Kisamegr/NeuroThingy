0058fa6cae43c71599ad82ff513a63e3f1ef91cb
Stratos Makrigiannakis <stratos.makrigiannakis@gmail.com>
2017-08-05 17:38:40 +0300

    Added missing variables and functions

--- Assets/Creature.cs
+++ Assets/Creature.cs
@@ -1,1 +1,27 @@
-using System.Collections;using System.Collections.Generic;using UnityEngine;public class Creature : MonoBehaviour{	public NeuralJoint[] joints;	public 	void Start() {	}	void Update() {	}}
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature : MonoBehaviour{
+    public NeuralJoint[] joints;
+    public Vector2 centroid;
+    public int generationCreated;
+    public float fitness;
+    public bool mutated;
+    public int muscleStrength;
+
+    public NeuralNet neuralNet;
+
+    public void Awake() {
+        
+    }
+
+    public void Think() {
+
+    }
+
+    public void ResetCreature() {
+        
+    }
+
+}
